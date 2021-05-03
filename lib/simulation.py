cb269192fdb83145a023446c80ac41d5a73cf7fe
wilsonsuen <waiyin.suen@sjsu.edu>
2021-05-03 00:44:35 -0700

    change test data handling

--- lib/simulation.py
+++ lib/simulation.py
@@ -4,6 +4,7 @@ import lgsvl
 import time
 import sys
 import logging
+import json
 from environs import Env
 from robot.api.deco import keyword
 from robot.api.logger import *
@@ -54,7 +55,8 @@ class Simulation(object):
             obj.transform = self.sim.get_spawn()[0]
         elif 'position' in position_data:
             obj.transform.position = lgsvl.Vector(position_data['position'])
-            obj.transform.rotation = lgsvl.Vector(position_data['rotation'])
+            if 'rotation' in position_data:
+                obj.transform.rotation = lgsvl.Vector(position_data['rotation'])
         if 'forward' in position_data or 'right' in position_data:
             forward = lgsvl.utils.transform_to_forward(offset_from.transform)
             right = lgsvl.utils.transform_to_right(offset_from.transform)
@@ -70,8 +72,8 @@ class Simulation(object):
         if 'opposite' in position_data and position_data['opposite']:
             obj.transform.rotation = lgsvl.Vector(obj.transform.rotation.x, obj.transform.rotation.y-180, obj.transform.rotation.z)
 
-    @keyword('Setup Scenario')
-    def setup_map_env(self, sim_map, ego, npcs=[], pedestrians=[]): # could be more like trafficlight
+    @keyword('Setup Scenario Old')
+    def setup_map_env_old(self, sim_map, ego, npcs=[], pedestrians=[]): # could be more like trafficlight
         """ This function set up map enviroment initial stage, user can define:
         Map(sim_map) - Map id for the map in test
         EGO car(ego) - {model, starting point, destination point}
@@ -121,6 +123,103 @@ class Simulation(object):
                 waypoints.append(lgsvl.DriveWaypoint(tempState.position, mph_to_mps(waypoint['speed']), npcState.transform.rotation))
             self.npcs[-1].follow(waypoints, loop=False)
             self.npcs[-1].on_collision(on_collision)
+
+    @keyword('Setup Scenario')
+    def setup_map_env(self, path): # could be more like trafficlight
+        """ This function set up map enviroment initial stage, user can define:
+        all configuration should be defined in a json file. Format follows output
+        of visual editor.
+        path: path to the test data json file
+        """
+        with open(path) as f:
+            testdata = json.load(f)
+        # Setup Simulator and Map
+        info(f"Setup Map: {testdata['map']['name']}", also_console=self.console)
+        self.set_simulator_and_map(testdata['map']['id'])
+        self.reset_map()
+
+        self.sim.set_time_of_day(testdata['time'])
+        self.sim.weather = lgsvl.WeatherState(*testdata['weather'])
+
+        ego = dict()
+        npcs = list()
+        peds = list()
+
+        for agent in testdata['agents']:
+            if agent['type'] == 1:
+                ego = agent
+            elif agent['type'] == 2:
+                npcs.append(agent)
+            elif agent['type'] == 3:
+                peds.append(agent)
+            else:
+                warning(f"Unknown type: {agent['variant']}")
+
+        # Setup EGO vehicle initial state
+        info(f"Setup EGO vehicle: {ego['variant']}", also_console=self.console)
+        info(f"Setup EGO Starting Point: {ego['transform']}", also_console=self.console)
+        self.egoState = lgsvl.AgentState()
+        self.state_point_handler(self.egoState, ego['transform'], self.egoState)
+        self.ego_model = self.env.str("LGSVL__VEHICLE_0", ego['sensorsConfigurationId'])
+        self.ego = self.sim.add_agent(self.ego_model, lgsvl.AgentType.EGO, self.egoState)
+        self.ego.connect_bridge(self.apollo_host, self.apollo_port)
+        self.ego.on_collision(on_collision)
+
+        # Connect Simulator, EGO and Dreamview
+        info(f"Connecting Dreamview: {self.apollo_host}:{self.apollo_port}", also_console=self.console)
+        self.dv = lgsvl.dreamview.Connection(self.sim, self.ego, self.apollo_host)
+        self.dv.set_hd_map(self.env.str("LGSVL__AUTOPILOT_HD_MAP", testdata['map']['name']))
+        self.dv.set_vehicle(self.env.str("LGSVL__AUTOPILOT_0_VEHICLE_CONFIG", ego['variant']))
+        info(f"Setup EGO modules: {ego['modules']}", also_console=self.console)
+        try:
+            modules = self.env.list("LGSVL__AUTOPILOT_0_VEHICLE_MODULES", subcast=str)
+            if len(modules) == 0:
+                modules = ego['modules']
+        except Exception:
+            modules = ego['modules']
+        egoDesState = lgsvl.AgentState()
+        info(f"Setup EGO Destination Point: {ego['destinationPoint']}", also_console=self.console)
+        self.state_point_handler(egoDesState, ego['destinationPoint'], self.egoState)
+        destination = egoDesState.transform.position
+        self.dv.setup_apollo(destination.x, destination.z, modules)
+        
+        info("Setup NPCs", also_console=self.console)
+        for npc in npcs:
+            info(f"Setup NPC: {npc['variant']}", also_console=self.console)
+            npcState = lgsvl.AgentState()
+            self.state_point_handler(npcState, npc['transform'], self.egoState)
+            self.npcs.append(self.sim.add_agent(npc['variant'], lgsvl.AgentType.NPC, npcState))
+            waypoints = list()
+            for waypoint in npc['waypoints']:
+                tempState = lgsvl.AgentState()
+                self.state_point_handler(tempState, waypoint, npcState)
+                if 'angle' in waypoint:
+                    angle = lgsvl.Vector(waypoint['angle'])
+                else:
+                    angle = npcState.transform.rotation
+                waypoints.append(lgsvl.DriveWaypoint(tempState.position, waypoint['speed'], angle, idle=waypoint['waitTime']))
+            self.npcs[-1].follow(waypoints, loop=False)
+            self.npcs[-1].on_collision(on_collision)
+
+        info(f"Setup Pedestrians", also_console=self.console)
+        for ped in peds:
+            info(f"Setup NPC: {ped['variant']}", also_console=self.console)
+            pedState = lgsvl.AgentState()
+            self.state_point_handler(pedState, ped['transform'], self.egoState)
+            self.npcs.append(self.sim.add_agent(ped['variant'], lgsvl.AgentType.NPC, pedState))
+            waypoints = list()
+            for waypoint in npc['waypoints']:
+                tempState = lgsvl.AgentState()
+                self.state_point_handler(tempState, waypoint, pedState)
+                if 'angle' in waypoint:
+                    angle = lgsvl.Vector(waypoint['angle'])
+                else:
+                    angle = lgsvl.Vector(0, 0, 0)
+                waypoints.append(lgsvl.DriveWaypoint(tempState.position, waypoint['speed'], angle, idle=waypoint['waitTime']))
+            self.npcs[-1].follow(waypoints, loop=False)
+            self.npcs[-1].on_collision(on_collision)
+
+
 
     @keyword
     def start_simulation(self):
--- testcases/school_bus.py
+++ testcases/school_bus.py
@@ -1,25 +1,25 @@
 import sys
 import os
+import glob
+import json
 from robot import rebot
 from robot.api import TestSuite
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from data.testdata.school_bus import TESTDATA, EGO_DATA, SCHOOL_BUS_DATA
 
 if __name__ == "__main__":
+    
     main_suite = TestSuite('School Bus Scenario')
     main_suite.resource.imports.library('lib/simulation.py')
 
-    for testdata in TESTDATA:
-        lane = testdata['school bus position']
-        status = testdata['school bus status']
-        npc = [{
-            'npc': 'SchoolBus',
-            'Starting Point': SCHOOL_BUS_DATA[lane]['Starting Point'],
-            'waypoints': SCHOOL_BUS_DATA[lane]['waypoints'] if status.lower() == 'moving' else\
-                            SCHOOL_BUS_DATA[lane]['waypoints'][:1]
-        }]
-        school_bus_test = main_suite.tests.create(f'Test encounter school bus {status} on {lane}')
-        school_bus_test.setup.config(name='Setup Scenario', args=["Straight2LaneOpposingPedestrianCrosswalk", EGO_DATA, npc])
+    testcase_paths = glob.glob('data/testdata/04_school_bus/*.json')
+    testcase_paths.sort()
+
+    for testcase_path in testcase_paths:
+        with open(testcase_path) as f:
+            testdata = json.load(f)
+        school_bus_test = main_suite.tests.create(testdata['testcase']['name'])
+        school_bus_test.setup.config(name='Setup Scenario2', args=[testcase_path])
         school_bus_test.body.create_keyword('Start Simulation')
         school_bus_test.teardown.config(name='Close Simulation')
 
